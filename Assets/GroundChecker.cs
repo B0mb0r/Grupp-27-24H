ba6f4cf3400ddab4eb1e4f5086518ad85bd93931
hugo fagerlind <hugo.fagerlind@elev.ga.lbs.se>
2019-01-23 15:09:45 +0100

    PlayerMovement

---
+++ Assets/GroundChecker.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundChecker : MonoBehaviour
+{
+    public static int JumpCounter;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Våran variabel som kollar om vi är på marken sätts som sann
+        JumpCounter = 2;
+    }
+}
