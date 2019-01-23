fad31710b8bb7f485512eaf80e263106e46942b7
richard.nittfors <richard.nittfors@elev.ga.lbs.se>
2019-01-23 16:46:44 +0100

    sewblade and timer done

---
+++ Assets/Script/Sawblade.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sawblade : MonoBehaviour
+{
+    public float movespeed;
+    private Rigidbody2D rbody;
+    public bool left = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (left == true)
+        {
+            rbody.velocity = -(Vector2)transform.right * movespeed;
+        }
+        else
+        {
+            rbody.velocity = (Vector2)transform.right * movespeed;
+        }
+    }
+}
