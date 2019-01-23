6e86b5765872e0c6003edf80d213e7e2858d8f61
felix.emblad <felix.emblad@elev.ga.lbs.se>
2019-01-23 16:01:16 +0100

    Felix.Crushingstone

---
+++ Assets/Enemyupdown.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemyupdown : MonoBehaviour
+{
+    public float speed = 1f;
+    public float upspeed = 1f;
+    public bool Down = true;
+    private Rigidbody2D rbody;
+
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+        if (Down == true)
+        {
+            rbody.velocity = -(Vector2)transform.up * speed;
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else
+        {
+            rbody.velocity = (Vector2)transform.up * upspeed;
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+    }
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Inviseblewall")
+        {
+            Down = !Down;
+        }
+    }
+}
