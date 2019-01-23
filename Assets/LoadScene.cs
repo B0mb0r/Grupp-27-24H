24b72e7028046333097794e9692c50b8cdff9e8e
felix.emblad <felix.emblad@elev.ga.lbs.se>
2019-01-23 14:53:36 +0100

    AciTrap

---
+++ Assets/LoadScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+  
+  
+    public string sceneToLoad = ("Felix.Sandbox");
+   
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+          
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+}
