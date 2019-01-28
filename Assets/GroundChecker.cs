fbf7405d69043b9ff71b0815074940fba23bcf28
hugo fagerlind <hugo.fagerlind@elev.ga.lbs.se>
2019-01-28 10:13:37 +0100

    PlayerMovement
    Saknar hög hopp och 1 dash i luften

---
+++ Assets/GroundChecker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundChecker : MonoBehaviour
+{
+    public bool isGrounded;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Våran variabel som kollar om vi är på marken sätts som sann
+        isGrounded = true;
+    }
+    //När jag lämnar kontakten med triggern så händer något
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        //Variabeln byts till falsk
+        isGrounded = false;
+    }
+}
